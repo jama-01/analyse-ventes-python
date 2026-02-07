b58e44d198c95fac0c2c1442ecb5ce2e8d0c9b7a
Mathieu KERBEL <mathieukerbel@gmail.com>
2026-02-07 16:01:54 +0100

    refactor: change for loop for sum iterator and dict iterator
    added lambda iteration and reduce the scope iteration
    
    Signed-off-by: Mathieu KERBEL <mathieukerbel@gmail.com>

--- analysis.py
+++ analysis.py
@@ -1,9 +1,7 @@
 import os
 
 base_dir = os.path.dirname(__file__)
-
 csv_path = os.path.join(base_dir, "data", "ventes.csv")
-
 donnees = []
 
 with open(csv_path, "r", encoding="utf-8") as file:
@@ -14,48 +12,25 @@ with open(csv_path, "r", encoding="utf-8") as file:
             colonnes[3] = float(colonnes[3])
             donnees.append(colonnes)
 
-total_ca = 0
-nb_commande = 0
-
-for line in donnees:
-    montant = line[3]
-    total_ca = total_ca + montant
-    nb_commande += 1
-
+total_ca = sum(line[3] for line in donnees)
+nb_commande = len(donnees)
 panier_moyen = total_ca / nb_commande
 
-ventes_par_pays = {}
+ventes_par_pays = {
+    pays: {
+        "commandes": sum(1 for line in donnees if line[2] == pays),
+        "total": sum(line[3] for line in donnees if line[2] == pays)
+    }
+    for pays in set(line[2] for line in donnees)
+}
 
-for line in donnees:
-    pays = line[2]
-    montant = line[3]
-    if pays not in ventes_par_pays:
-        ventes_par_pays[pays] = {
-            "commandes": 0,
-            "total": 0
-        }
-
-    ventes_par_pays[pays]["commandes"] += 1
-    ventes_par_pays[pays]["total"] += montant
-
-ventes_par_client = {}
-
-for line in donnees:
-    client = line[1]
-    montant = float(line[3])
-    if client not in ventes_par_client:
-        ventes_par_client[client] = {
-            "total": 0
-        }
-    ventes_par_client[client]["total"] += montant
-
-max_client = None
-max_total = 0
-
-for client, i in ventes_par_client.items():
-    if i["total"] > max_total:
-        max_total = i["total"]
-        max_client = client
+ventes_par_client = {
+    client: {
+        "total": sum(line[3] for line in donnees if line[1] == client)
+    }
+    for client in set(line[1] for line in donnees)
+}
+max_client, max_total = max(ventes_par_client.items(), key=lambda item: item[1]["total"])
 
 print("\n============================== \nRAPPORT D'ANALYSE DES VENTES \n==============================")
 
